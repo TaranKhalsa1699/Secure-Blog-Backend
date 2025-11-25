9ff2684af1556c1d9a28a076357597dc0ab67e75
Taranpreet Singh <preetaran2002@gmail.com>
2025-11-25 22:56:11 +0530

    5

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+def home(request):
+    return JsonResponse({"message": "Welcome to Secure Blog API"})
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/auth/register/', include('accounts.urls')),
