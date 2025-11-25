d24fd2a0767667019085c67eea5d111cd65297f4
Taranpreet Singh <preetaran2002@gmail.com>
2025-11-25 20:04:05 +0530

    your

--- secure-blog-backend/accounts/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-# No custom model needed
-
-# Create your models here.
--- secure-blog-backend/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PostListCreate, PostDetail, CommentListCreate
-
-urlpatterns = [
-    path("posts/", PostListCreate.as_view()),
-    path("posts/<int:pk>/", PostDetail.as_view()),
-    path("comments/", CommentListCreate.as_view()),
-]
